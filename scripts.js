b0dbeae1d6a0ad1b430db425fc6f85ec01d53a29
Thabang Selowa <mohale20cobra@gmail.com>
2024-09-05 22:39:02 +0200

    Add logical OR operator to evaluate condionts

--- scripts.js
+++ scripts.js
@@ -3,7 +3,17 @@ const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  // Check for empty inputs return results when requirement are not met
+  if (!dividend || !divider) {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again.";
+    return;
+  }
+  
+  // Check if inputs are valid numbers
+ 
+ 
 });
